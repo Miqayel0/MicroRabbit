4c42461aa6b8493d2f9da806f2243e6f7be89d46
Miqayel Avagyan <miqayel.avagyan@outlook.com>
2020-12-04 01:43:41 +0400

    added RabbitMQConnection

--- MicroRabbit/EventBusRabbitMQ/EventBusRabbitMQ.cs
+++ MicroRabbit/EventBusRabbitMQ/EventBusRabbitMQ.cs
@@ -1,13 +1,76 @@
 ﻿using MicroRabbit.EventBus.Abstractions;
 using MicroRabbit.EventBus.Events;
+using Microsoft.Extensions.Logging;
+using Polly;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
+using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
 
 namespace MicroRabbit.EventBusRabbitMQ
 {
     public sealed class EventBusRabbitMQ : IEventBus
     {
+        const string BROKER_NAME = "microrabbit_event_bus";
+
+        private readonly IRabbitMQPersistentConnection _persistentConnection;
+        private readonly ILogger<EventBusRabbitMQ> _logger;
+
+        private readonly int _retryCount;
+
+        private readonly IModel _consumerChannel;
+
+        public EventBusRabbitMQ(IRabbitMQPersistentConnection persistentConnection, ILogger<EventBusRabbitMQ> logger, int retryCount, IModel consumerChannel)
+        {
+            _persistentConnection = persistentConnection ?? throw new ArgumentNullException(nameof(persistentConnection));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _retryCount = retryCount;
+            _consumerChannel = consumerChannel;
+        }
+
         public void Publish(IntegrationEvent @event)
         {
-            throw new System.NotImplementedException();
+            if (!_persistentConnection.IsConnected)
+            {
+                _persistentConnection.TryConnect();
+            }
+
+            var policy = Policy.Handle<BrokerUnreachableException>()
+                .Or<SocketException>()
+                .WaitAndRetry(_retryCount, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)), (ex, time) =>
+                {
+                    _logger.LogWarning(ex, "Could not publish event: {EventId} after {Timeout}s ({ExceptionMessage})", @event.Id, $"{time.TotalSeconds:n1}", ex.Message);
+                });
+
+            var eventName = @event.GetType().Name;
+
+            _logger.LogTrace("Creating RabbitMQ channel to publish event: {EventId} ({EventName})", @event.Id, eventName);
+
+            using var channel = _persistentConnection.CreateModel();
+
+            _logger.LogTrace("Declaring RabbitMQ exchange to publish event: {EventId}", @event.Id);
+
+            channel.ExchangeDeclare(exchange: BROKER_NAME, type: "direct");
+
+            var message = JsonSerializer.Serialize(@event);
+            var body = Encoding.UTF8.GetBytes(message);
+
+            policy.Execute(() =>
+            {
+                var properties = channel.CreateBasicProperties();
+                properties.DeliveryMode = 2; // persistent
+
+                    _logger.LogTrace("Publishing event to RabbitMQ: {EventId}", @event.Id);
+
+                channel.BasicPublish(
+                    exchange: BROKER_NAME,
+                    routingKey: eventName,
+                    mandatory: true,
+                    basicProperties: properties,
+                    body: body);
+            });
         }
 
         public void Subscribe<T, TH>()
---
+++ MicroRabbit/EventBusRabbitMQ/IRabbitMQPersistentConnection.cs
@@ -0,0 +1,14 @@
+﻿using RabbitMQ.Client;
+using System;
+
+namespace MicroRabbit.EventBusRabbitMQ
+{
+    public interface IRabbitMQPersistentConnection : IDisposable
+    {
+        bool IsConnected { get; }
+
+        bool TryConnect();
+
+        IModel CreateModel();
+    }
+}
