b33b56fd88e45a1473ce8bfe8cc4d8132e50afe0
Miqayel <avag.miqayel@gmail.com>
2020-12-03 20:08:05 +0400

    commit

---
+++ MicroRabbit/EventBusRabbitMQ/EventBusRabbitMQ.cs
@@ -0,0 +1,27 @@
+﻿using MicroRabbit.EventBus.Abstractions;
+using MicroRabbit.EventBus.Events;
+
+namespace MicroRabbit.EventBusRabbitMQ
+{
+    public sealed class EventBusRabbitMQ : IEventBus
+    {
+        public void Publish(IntegrationEvent @event)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Subscribe<T, TH>()
+            where T : IntegrationEvent
+            where TH : IIntegrationEventHandler<T>
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Unsubscribe<T, TH>()
+            where T : IntegrationEvent
+            where TH : IIntegrationEventHandler<T>
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
